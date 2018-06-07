c00d4444119b26c3f7cc71bc82822be658841b34
liuabin <hzlxb123@gmail.com>
2018-06-07 22:40:47 +0800

    Add a .java in vscode.

---
+++ TempJava.java
@@ -0,0 +1,15 @@
+/**
+ * TempJava
+ */
+public class TempJava {
+    private int age;
+
+    public TempJava() {
+        System.out.println("Nothing.");
+        age = 20;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello, vscode!");
+    }
+}
