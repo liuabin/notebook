3600ee61e8ed663be6d93565bc8228f4e5fb8d00
liuabin <hzlxb123@gmail.com>
2018-06-12 22:36:41 +0800

    A new java note.

---
+++ Java/Exception.md
@@ -0,0 +1,15 @@
+# Java Exception
+
+1. 可查异常 CheckedException
+    * try-catch need
+    * FileNotFoundException
+
+2. 运行时异常 RuntimeException
+    * 错误很普遍，不强制try-catch捕获
+    * ArithmeticException -- 除数为0
+    * :ArrayIndexOutOfBoundsException
+    * NullPointerException
+
+3. 错误 Error
+    * 系统级别的异常
+    * OutOfMemoryError
--- TempJava.java
+++ TempJava.java
@@ -1,15 +1,42 @@
 /**
  * TempJava
  */
-public class TempJava {
-    private int age;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-    public TempJava() {
-        System.out.println("Nothing.");
-        age = 20;
-    }
+import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
+
+public class TempJava {
 
     public static void main(String[] args) {
-        System.out.println("Hello, vscode!");
+        FileInputTest f = new FileInputTest();
+        f.fileInput("f:\\temp.cpp");
+        // try {
+        // } catch (Exception e){
+        //     e.printStackTrace();
+        // }
+    }
+}
+
+/**
+ * FileInputTest
+ */
+class FileInputTest {
+
+    public void fileInput(String path) {
+        File f = new File(path);
+        try {
+            FileInputStream fis = new FileInputStream(f);
+            System.out.println("File opened successfully.");
+            System.out.println((char)(fis.read()));
+            fis.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            // throw(new Exception());
+        } finally {
+            // Do noting.
+        }         
     }
 }
